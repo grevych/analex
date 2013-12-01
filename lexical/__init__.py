836b1544c59e74b3d7afe4bfa237ccbd16358ba0
Gerardo Reyes <grevych@gmail.com>
2013-12-01 03:22:39 -0600

    primeros y siguientes

--- lexical/__init__.py
+++ lexical/__init__.py
@@ -7,6 +7,7 @@ import sys
 import re
 import util
 
+from collections import namedtuple
 
 
 class Lexical:
@@ -26,39 +27,197 @@ class Lexical:
         self.status = ""
         self.flags = {}
         self.debugger = debugger
-        self.lexemes_re = [re.compile(lxm['token']) for lxm in LEXEMES]
-        self.lexemes_ext_re = [re.compile(lxm['token']) for lxm in LEXEMES_EXT]
+        
+        
+        TokenWithDelimiter = namedtuple('Token', ('name', 'regexp', 'regexp_at_start', ))
+        Token = namedtuple('Token', ('name', 'regexp', ))
+        
+        self.tokens = [Token(
+                            token[0], 
+                            re.compile(token[1])) for token in util.TOKENS]
+                            
+        self.tokens_with_del = [TokenWithDelimiter(
+                                    token[0], 
+                                    re.compile(token[1]), 
+                                    re.compile(token[2])) for token in util.TOKENS_WITH_DELIMITERS]
         #self.debugger.print_msg("Análisis léxico >")
+        self.t = []
         
         
     def run(self):
         with self.source:
             buffer_str = ""
-            
+            buffer_str_del = ""
+            is_token_with_del = False
+
             for char in iter(lambda: self.source.read(1), ''):
-                sys.stdout.write(char)
+                #sys.stdout.write(char)
+ 
+                if not char.isspace():
+                    buffer_str += char
+                    continue
+                    
                 
-    
+                
+                if is_token_with_del:
+                    buffer_str_del += buffer_str + char
+                    
+                    for token in self.tokens_with_del:
+                        match = token.regexp.match(buffer_str_del)
+                        
+                        if match:
+                            print match.group()
+                            self.t.append(token.name)
+                            buffer_str_del = buffer_str = ""
+                            break
+                else:
+                    
+                    for token in self.tokens_with_del:
+                        match = token.regexp_at_start.match(buffer_str)
+                        
+                        if match:
+                            print match.group() + " NUEVO EN DELIM"
+                            is_token_with_del = True
+                            buffer_str_del = buffer_str
+                            buffer_str = ""
+                            break
+                            
+                        is_token_with_del = False
+                    
+                    if not is_token_with_del:
+                        for token in self.tokens:
+                            match = token.regexp.match(buffer_str)
+
+                            if match:
+                                print match.group() + " NUEVO"
+                                self.t.append(token.name)
+                                buffer_str = ""
+                                break
+                
+                print buffer_str + "|ACEPTADO?"
+
+        print self.t
 
 
 
-
-
-
+    def anasin(self):        
+        #primeros y siguientes
+        if not util.GRAMMAR.__len__():
+            #error no existe gramatica
+            return
+            
+        self.grammar = [{production[0]: production[1]} for production in util.GRAMMAR]
+        self.first = {}
+        self.next = {}
+        
+        
+        for production in util.GRAMMAR:
+            self.get_first(production[0])
+            self.get_next(production[0])
+            
+                
+        print self.grammar
+        print self.first
+        print self.next
     #try finally raise except assert
+    
+    
+    
+    def get_first(self, key):
+        if self.first.has_key(key):
+            return self.first[key]
+        
+        self.first[key] = []
+        
+        for production in self.grammar:
+            if production.has_key(key):
+                elements = production[key].split()
+                if elements[0] != key:
+                    if elements[0].islower():
+                        self.first[key].append(elements[0])
+                    else:
+                        epsilons = 0
+                        is_epsilon = False
+                        
+                        for element in elements:
+                            for first in self.get_first(element):
+                                if not first.find('epsilon'):
+                                    epsilons = epsilons + 1
+                                    is_epsilon = True
+                                    continue
+                                    
+                                if not first in self.first[key]:
+                                    self.first[key].append(first)
+                            
+                            if not is_epsilon:
+                                break
+                                
+                            is_epsilon = False
+                        
+                        if epsilons == elements.__len__():
+                            if not 'epsilon' in self.first[key]:
+                                self.first[key].append('epsilon')
+                                
+        return self.first[key]
+            
+        
+
+    def get_next(self, key):
+        if self.next.has_key(key):
+            return self.next[key]
+            
+        self.next[key] = []
+        
+        if key == 'A' and '$' not in self.next[key]:
+            self.next[key].append('$')   
+        
+        for production in self.grammar:
+            elements = production.values()[0].split()
+            index = 0
+            
+            for element in elements:
+                if element.__eq__(key):
+                    if index == elements.__len__() - 1:
+                        if production.keys()[0] != key:
+                            for next in self.get_next(production.keys()[0]):
+                                if next not in self.next[key]:
+                                    self.next[key].append(next)
+                    else:
+                        if elements[index + 1].islower():
+                            self.next[key].append(elements[index + 1])
+                        else:
+                            is_epsilon = False
+                            for next in self.get_first(elements[index + 1]):
+                                if not next.find('epsilon'):
+                                    is_epsilon = True
+                                    continue
+                                if next not in self.next[key]:
+                                    self.next[key].append(next)
+                                    
+                            if is_epsilon:
+                                for next in self.get_next(production.keys()[0]):
+                                    if next not in self.next[key]:
+                                        self.next[key].append(next)
+                        
+                index = index + 1
+                        
+        return self.next[key]
+        
 
 
 
 lex = Lexical("prueba", None)
+lex.run()
+lex.anasin()
 
-if lex:
-    print lex
-    pass 
+# if lex:
+#     print lex
+#     pass 
     #lex.run()
-
+info = 'ANALEX'
 if __name__ == "__main__":                 
-    print info.__doc__
-
+    #print info.__doc__
+    pass
     
     
     
--- lexical/util.py
+++ lexical/util.py
@@ -1,41 +1,64 @@
 
-
 DELIMITERS = r'\s'
 
 
 #Lexema (nombre, regexp)
-LEXEMES = (
-    {'name':'AUTO', 'token': r'auto'},
-    {'name':'BREAK', 'token': r'break'},
-    {'name':'CASE', 'token': r'case'},
-    {'name':'CHAR', 'token': r'char'},
-    {'name':'CONST', 'token': r'const'},
-    {'name':'CONTINUE', 'token': r'continue'},
-    {'name':'DEFAULT', 'token': r'default'},
-    {'name':'DO', 'token': r'do'},
-    {'name':'DOUBLE', 'token': r'double'},
-    {'name':'ELSE', 'token': r'else'},
-    {'name':'ASIG_SUM', 'token': r'+='},
-    {'name':'ASIG_RES', 'token': r'-='},
-    {'name':'ASIG_MUL', 'token': r'*='},
-    {'name':'ASIG_DIV', 'token': r'/='},
-    {'name':'ASIG_MOD', 'token': r'%='},
-    {'name':'DIV', 'token': r'/'},
-    {'name':'MUL', 'token': r'*'},
-    {'name':'SUM', 'token': r'+'},
-    {'name':'RES', 'token': r'-'},
-    {'name':'MOD', 'token': r'%'},
-    {'name':'ASIG', 'token': r'='},
-    {'name':'ID', 'token': r'[a-zA-Z0_][\w]*'},
+TOKENS = (
+    ('DECIMAL',  r'[+-]?[0-9]*(\.)[0-9]+(E[+-]?[0-9]+)?'),
+    ('ENTERO',  r'[+-]?[0-9]+'),
+    ('AUTO',  r'auto'),
+    ('BREAK',  r'break'),
+    ('CASE',  r'case'),
+    ('CHAR',  r'char'),
+    ('CONST',  r'const'),
+    ('CONTINUE',  r'continue'),
+    ('DEFAULT',  r'default'),
+    ('DO',  r'do'),
+    ('DOUBLE',  r'double'),
+    ('ELSE',  r'else'),
+    ('ASIG_SUM',  r'\+='),
+    ('ASIG_RES',  r'\-='),
+    ('ASIG_MUL',  r'\*='),
+    ('ASIG_DIV',  r'/='),
+    ('ASIG_MOD',  r'%='),
+    ('DIV',  r'/'),
+    ('MUL',  r'\*'),
+    ('SUM',  r'\+'),
+    ('RES',  r'\-'),
+    ('MOD',  r'%'),
+    ('ASIG',  r'='),
+    ('ID',  r'[a-zA-Z0_][\w]*'),
+
 
     
 )
 
+#r'/\*([^*]|[\s]|(\*+([^*/]|[\s])))*\*+/'
 
-EXT_LEXEMES = (
-    {'name':'STRING', 'token': r'',  'start':r'', 'end':r''},
-    {'name':'COMMENT_BL', 'token': r'', 'start':r'', 'end':r''},
-    {'name':'COMMENT_LN', 'token': r'', 'start':r'', 'end':r''},
+
+TOKENS_WITH_DELIMITERS = (
+#    ('STRING', r'a', r'a'),
+    ('COMMENT_BLOCK', r'/\*([^*]|[\s]|(\*+([^/]|[\s])))*\*+/', r'/\*([^*]|[\s]|(\*+([^/]|[\s])))*\*+/'),
+    ('COMMENT_BLOCK', r'/\*([^*]|[\s]|(\*+([^/]|[\s])))*\*+/', r'/\*([^*]|[\s]|(\*+([^/]|[\s])))*'),
+    ('COMMENT_BLOCK', r'/\*([^*]|[\s]|(\*+([^/]|[\s])))*\*+/', r'/\*'),
+#    ('COMMENT_NEW_LINE', r'a', r'a'),
     
 )
+
+
+
+
+GRAMMAR = (
+    ('A', 'id asig E'),
+    ('E', 'T R'),
+    ('R', 'mas E'),
+    ('R', 'epsilon'),
+    ('T', 'F Q'),
+    ('Q', 'mul T'),
+    ('Q', 'epsilon'),
+    ('F', 'id'),
+    ('F', 'cte'),
+    
+)
+
 
